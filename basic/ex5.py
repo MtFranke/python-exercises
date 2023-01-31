01530b63c88a969b2f479c3a3dbbcc67f5a899ad
mateuszfranke <mateuszfranke@icloud.com>
2023-01-31 17:00:12 +0100

    excersises 6

---
+++ basic/ex5.py
@@ -0,0 +1,6 @@
+# Write a Python program that accepts the user's first and last name and prints them in reverse order with a space between them.
+
+name = input("Please enter your name : ")
+lastname = input("Please enter your lastname : ")
+
+print(f"Hello {lastname} {name}, good to see you today.")
