ad4ce107efee7592ee94f5b63a20027b0e436d06
mateuszfranke <mateuszfranke@icloud.com>
2023-02-02 07:46:12 +0100

    exercise 8

---
+++ basic/ex8.py
@@ -0,0 +1,7 @@
+# Write a Python program to display the first and last colors from the following list.
+# color_list = ["Red","Green","White" ,"Black"]
+
+color_list = ["Red", "Green", "White", "Black"]
+print(f"First color is {color_list[0]}.")
+color_list.reverse()
+print(f"Last color is {color_list[0]}.")
