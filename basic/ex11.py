ea22bceb1328fd5d606009f88861e8b5c301d900
mateuszfranke <mateuszfranke@icloud.com>
2023-02-03 18:38:05 +0100

    exercise 11, built-in functions p1

---
+++ basic/ex11.py
@@ -0,0 +1,10 @@
+"""
+Write a Python program to print the documents (syntax, description etc.) of Python built-in function(s).
+Sample function : abs()
+Expected Result :
+abs(number) -> number
+Return the absolute value of the argument.
+"""
+
+print(f"abs({-6}) -> {abs(6)}, Return the absolute value of the argument.")
+print(f' [1,2,3,4].any(1) -> object , Return true if collection contains given element, false if element dont exist')
