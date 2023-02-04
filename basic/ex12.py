f3bcbc21426b60af6108d8030f45c82507572863
mateuszfranke <mateuszfranke@icloud.com>
2023-02-04 15:13:47 +0100

    exercise 12

---
+++ basic/ex12.py
@@ -0,0 +1,10 @@
+"""
+Write a Python program that prints the calendar for a given month and year.
+Note : Use 'calendar' module.
+"""
+import calendar
+
+month = 2
+year = 2023
+
+print(calendar.month(year, month))
