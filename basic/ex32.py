95af5ac2f837e46fdafa309dc278a36fa40e888a
mateuszfranke <mateuszfranke@icloud.com>
2023-02-07 18:18:07 +0100

    exercise 32

---
+++ basic/ex32.py
@@ -0,0 +1,9 @@
+"""
+Write a Python program to find the least common multiple (LCM) of two positive integers.
+"""
+import math
+
+a = 12
+b = 6
+
+print(f"Least common multiple of {a} and {b} is {math.lcm(a, b)}")
