6d3a5420357ed149405974045c5fbfb56af6d3f0
mateuszfranke <mateuszfranke@icloud.com>
2023-02-19 12:22:53 +0100

    exercise 49

---
+++ basic/ex49.py
@@ -0,0 +1,9 @@
+"""
+Write a Python program to list all files in a directory.
+https://stackoverflow.com/questions/3207219/how-do-i-list-all-files-of-a-directory
+"""
+
+import os
+
+arr = os.listdir()
+print(arr)
